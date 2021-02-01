60fd47d7a26f62cede8c1cc79220a50171413873
D0m1nc <39738012+D0m1nc@users.noreply.github.com>
2021-02-01 20:44:13 +0200

    Jump
    Idejau kad galetu pasokti

--- Sademon/Assets/Scripts/PlayerController.cs
+++ Sademon/Assets/Scripts/PlayerController.cs
@@ -11,13 +11,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private bool facingRight = true;
 
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpValue;
+
     void Start()
     {
+        extraJumps = extraJumpValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+
         moveInput = Input.GetAxis("Horizontal");
         Debug.Log(moveInput);
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
@@ -31,6 +45,30 @@ public class PlayerController : MonoBehaviour
             flip();
         }
     }
+
+
+    void Update()
+    {
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+
+
+    }
+
+
 
     void flip()
     {
