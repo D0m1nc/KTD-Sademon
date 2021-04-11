ebbaeaad888a15712c6bcc2c2266dbffa64d5a7a
D0m1nc <39738012+D0m1nc@users.noreply.github.com>
2021-04-11 18:56:30 +0300

    Platformos
    Pakeiciau platformas ir colliderius

---
+++ Sademon/Assets/Scripts/healthBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBar : MonoBehaviour
+{
+    public float CurrentHealth;
+    private float MaxHealth = 100f;
+    PlayerController Player;
+    private void Start()
+    {
+        Player = FindObjectOfType<PlayerController>();
+    }
+    private void Update()
+    {
+        CurrentHealth = Player.Health;
+    }
+}
